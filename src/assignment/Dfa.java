03d8dad67dfdcfd9283c04f76326a065276e1ca2
talhavawda <talhavawda.dev@gmail.com>
2020-11-22 18:22:05 +0200

    All bugs fixed; getting correct output

--- src/assignment/Dfa.java
+++ src/assignment/Dfa.java
@@ -234,9 +234,9 @@ public class Dfa {
 	 */
 	public static HashSet<NfaState> epsilonClosure(HashSet<NfaState> nfaStates) {
 
-		HashSet<NfaState> ec = (HashSet<NfaState>) nfaStates.clone();
+		HashSet<NfaState> epsilonClosure = (HashSet<NfaState>) nfaStates.clone();
 		//...
-		Stack<NfaState> epsilonStates = new Stack<NfaState>();
+		Stack<NfaState> epsilonStatesStack = new Stack<NfaState>();
 		for (NfaState n : nfaStates){
 			/*
 			getNext1() corresponds to getSymbol()
@@ -244,12 +244,14 @@ public class Dfa {
 			i.e. you transition from current state to getNext1() on getSymbol1() (or range [getSymbol1(), getSymbol2()]
 			 */
 
+			/*
 			// NFA created such that states null by default and symbol2 set to epsilon => check if both there is a next state and if you transition to it on epsilon
-			if (n.getNext1() != null && n.getSymbol() == NfaState.EPSILON || n.getNext2() != null && n.getSymbol2() == NfaState.EPSILON ) {
-				if (n.getNext1() != null && n.getSymbol() == NfaState.EPSILON ) epsilonStates.push(n.getNext1());
-				if (n.getNext2() != null && n.getSymbol2() == NfaState.EPSILON ) epsilonStates.push(n.getNext2());
+			if (n.getNext1() != null && n.getSymbol() == NfaState.EPSILON ) epsilonStates.push(n.getNext1());
+			if (n.getNext2() != null && n.getSymbol2() == NfaState.EPSILON ) epsilonStates.push(n.getNext2());
 
 				// while you can still find epsilon transitions for this specific path
+
+
 				while (!epsilonStates.empty()){
 					NfaState nextState = epsilonStates.pop();
 					ec.add(nextState);
@@ -257,11 +259,29 @@ public class Dfa {
 					if (nextState.getNext2() != null && nextState.getSymbol2() == NfaState.EPSILON) epsilonStates.push(nextState.getNext1());
 				}
 
+				*/
+
+			epsilonStatesStack.push(n);
+
+			while (!epsilonStatesStack.empty()) {
+				NfaState t = epsilonStatesStack.pop();
+
+				//Because of Thompson's Construction, NfaState t has a max of 2 epsilon transitions from it
+
+				if (t.getNext1() != null && t.getSymbol() == NfaState.EPSILON) {
+					epsilonClosure.add(t.getNext1()); //add() will first check if next2 is already in it or not
+					epsilonStatesStack.push(t.getNext1());
+				}
+
+				if (t.getNext2() != null && t.getSymbol2() == NfaState.EPSILON) {
+					epsilonClosure.add(t.getNext2()); //add() will first check if next2 is already in it or not
+					epsilonStatesStack.push(t.getNext2());
+				}
 			}
 
 
 		}
-		return ec;
+		return epsilonClosure;
 	}
 
 
