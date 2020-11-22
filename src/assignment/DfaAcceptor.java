54f8265b0f7e5e22e3f5b8cd263110d9f14bf348
talhavawda <talhavawda.dev@gmail.com>
2020-11-22 11:51:43 +0200

    Bug fixing and adding comments to DfaAcceptor

--- src/assignment/DfaAcceptor.java
+++ src/assignment/DfaAcceptor.java
@@ -1,42 +1,61 @@
 package assignment;
 
+/**
+ * A class to determine if a string belongs to a particular language
+ * i.e. Determine if the DFA that represents this language accepts or rejects the string
+ *
+ * The class has a single field that is the Dfa for the language (it is passed in via the constructor)
+ * It has a boolean method accept() that takes in a string as a parameter and determines if the DFA should accept or reject the string
+ * i.e. it returns true if the DFA accepts the string, otherwise returns false
+ */
 public class DfaAcceptor {
 
-	/*
-		Constructor - take in as parameter a Dfa object and store it as a field
+	private Dfa dfa;
 
-		define a boolean method accept() with a string parameter that determines if the Dfa should accept the string or not
-			->> SEE THE DFA-RECOGNISE ALGORITHM IN THE COMP314 SLIDES
-			->> this method should use the transition table of the Dfa object to determine if the string must be accepted or ont
-				->> Study the DfaState and Dfa class (and also the Notes.txt file I made earlier) to understand how it works
-					->> Divya and I are not entirely completed with the code, but assume that everything works when coding this class
-						i.e. The transition table is created correctly etc.
 
-						q  = s0
-    x = 1st input symbol
-    while x ≠  eof  {
-         q  =  δ(q,x)
-         x  =  next input symbol
-     }
-     if q ∈ F
-         input recognised
-     else
-         input not recognised	 */
-	public Dfa dfa;
-	protected int stateAt=1;
+	/**
+	 * Constructor
+	 * @param dfa - the DFA representing the language
+	 */
 	public DfaAcceptor(Dfa dfa){
 		this.dfa=dfa;
 	}
 
+	/*
+		 Takes in a string as a parameter and determines if the DFA should accept or reject this string
+
+		 It uses the transition table of the Dfa object to determine if the string must be accepted or rejected
+
+		 DFA Accepting Algorithm:
+
+			q  = s0 (Start State)
+	        x = 1st input symbol from input string
+
+	        while x ≠  end of input string {
+	             q  =  δ(q,x)
+	             x  =  next input symbol
+	         }
+
+	         if q ∈ F (If q is an Accept State)
+	             input recognised (Accept the string)
+	         else
+	             input not recognised (Reject the string)
+	 */
 	public boolean accept(String input){
-		for(int i=0; i<input.length(); i++){
-			stateAt=dfa.transTable[stateAt][input.charAt(i)]; //stateAt is initially set to start state
-			if(stateAt==0){
-				return false; // checks if its in trap state
+
+		int stateAt = Dfa.START; //stateAt is initially set to the Start State
+
+		for (int i = 0; i < input.length(); i++){ //traverse through the input string - each input symbol at a time
+			stateAt =dfa.transTable[stateAt][input.charAt(i)]; //transition to the next state based on the current input symbol
+
+			if(stateAt == Dfa.TRAP) { // checks if its in trap state
+				return false; //Reject the input string
 			}
 		}
 
-		return stateAt == Dfa.ACCEPT;  //if (stateAt == Dfa.ACCEPT) then return true, else return false
+		//We have now finished reading in the input
+
+		return stateAt == Dfa.ACCEPT;  //if (stateAt == Dfa.ACCEPT) [i.e. the last state we in is an Accept State or not] then return true, else return false
 
 	}
 }
